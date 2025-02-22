43d9b65de0a74a4950923110319d4dcdf3bc1122
AssemSuper <asemk118@gmail.com>
2025-02-22 18:47:30 +0100

    13 commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,12 @@ const HomePage = () => (
     <>
     <button>Click Me</button>
     <h1>Hello, welcome to my blog!</h1>
+    <select>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+
     <p>
         Welcome to my blog! We can find about me. 
         Proin congue
