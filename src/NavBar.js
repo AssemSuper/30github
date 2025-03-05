8aba52079815f3e1a9a2a26c360e673c4389f96b
AssemSuper <asemk118@gmail.com>
2025-03-05 19:23:34 +0100

    24th Commit

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const NavBar = () => {
+    [navigate] = useNavigate();
     return(
         <nav>
             <ul>
