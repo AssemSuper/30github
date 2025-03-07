06e6b8fe849a7629e706cc03f51de73229889c05
AssemSuper <asemk118@gmail.com>
2025-03-07 20:17:47 +0100

    26th Commit

---
+++ src/components/Query.tsx
@@ -0,0 +1,7 @@
+const getActivity = async (): Promise<void> => {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await response.json();
+    console.log(data);
+    
+}
