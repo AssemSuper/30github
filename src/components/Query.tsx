5555c6faf9678538743760fba7e47ed3fda5e6aa
AssemSuper <asemk118@gmail.com>
2025-03-10 21:10:28 +0100

    29th Commit

--- src/components/Query.tsx
+++ src/components/Query.tsx
@@ -1,4 +1,6 @@
 import { AxiosResponse } from "axios";
+import { useEffect } from "react";
+const [activity, setActivity] = useState(false);
 const getActivity = async (): Promise<void> => {
 
     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
@@ -14,3 +16,7 @@ const addActivity = (){
 cont cancelActivity = async (endpoint:string): Promise<AxiosResponse<void>>  =>{
     const response= await axios.patch(endpoint,{state:'cancelled'},{headers:{'Content-Type':'application/json'}});
 }
+useEffect(() => {
+    setActivity(true);
+    
+})
