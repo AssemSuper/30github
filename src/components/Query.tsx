259bd349b165e38d0695646f0266f64ed306f864
AssemSuper <asemk118@gmail.com>
2025-03-11 20:14:55 +0100

    30th Commit

--- src/components/Query.tsx
+++ src/components/Query.tsx
@@ -1,5 +1,5 @@
-import { AxiosResponse } from "axios";
-import { useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
 const [activity, setActivity] = useState(false);
 const getActivity = async (): Promise<void> => {
 
@@ -9,14 +9,17 @@ const getActivity = async (): Promise<void> => {
     
 }
 
-const addActivity = (){
+const addActivity = () =>{
 
     
 }
-cont cancelActivity = async (endpoint:string): Promise<AxiosResponse<void>>  =>{
+const cancelActivity = async (endpoint:string): Promise<AxiosResponse<void>>  =>{
     const response= await axios.patch(endpoint,{state:'cancelled'},{headers:{'Content-Type':'application/json'}});
+    return response;
 }
 useEffect(() => {
+
     setActivity(true);
+    const storedActivities =JSON.parse(localStorage.getItem('activities') || '[]');
     
 })
